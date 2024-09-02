2fcc7af207487e25e1240555e178e207a11bfadb
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-09-01 22:14:31 -0300

    fix: apelido como unique.

--- db/migration/000001_init.up.sql
+++ db/migration/000001_init.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS pessoas (
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    apelido VARCHAR(32) NOT NULL,
+    apelido VARCHAR(32) NOT NULL UNIQUE,
     nome VARCHAR(100) NOT NULL,
     nascimento VARCHAR(10) NOT NULL,
     stack VARCHAR(32)[]
